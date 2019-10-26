d2e63fd8d9aa1f9ba7a53f492509978c047b63ab
sswietlik <sswietlik@gmail.com>
2019-10-26 15:31:36 +0200

    Zlokalizowano i wprowadzono parametry rubryki wyszukiwania

--- src/test/java/ZadanieDodatkowe_1/UnikniecieParadoksuPestycydow.java
+++ src/test/java/ZadanieDodatkowe_1/UnikniecieParadoksuPestycydow.java
@@ -1,4 +1,36 @@
 package ZadanieDodatkowe_1;
 
-public class UnikniecieParadoksuPestycydow {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UnikniecieParadoksuPestycydow   {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/WebDrivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://prod-kurs.coderslab.pl/index.php");
+    }
+    @Test
+    public void testGoogleSearch() {
+        WebElement element = driver.findElement(By.name("s"));
+        element.clear();
+        element.sendKeys("Mistrzostwa Świata w piłce noż");
+        element.submit();
+    }
+    @After
+    public void tearDown() throws Exception {
+        // Zamknij przeglądarkę
+//        driver.quit();
+    }
 }
